b0948128d5ace963fdc07a1e6e1f4bf456873885
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2021-01-16 01:53:16 +0900

    button

--- prepare/front/components/PostCardContent.js
+++ prepare/front/components/PostCardContent.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Input } from "antd";
+import { Button, Input } from "antd";
 import { useSelector } from "react-redux";
 
 const { TextArea } = Input;
